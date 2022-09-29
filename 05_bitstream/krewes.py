69f0bf3aeb51cacf8c6cfff5b9de515133943773
Julia Lee <jlee30@stuy.edu>
2022-09-29 16:30:08 -0400

    Added some comments

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -4,9 +4,15 @@ SoftDev
 K05 -- bitstream
 2022-09-29
 time spent:
+1 class period + 5 minutes outside of class for debugging
 DISCO:
-QCC:
+list.append()
+dictionary[key] = value
 
+QCC:
+I remember being told to close files after reading them, but I don't really remember or understand why that's important.
+Is there a way to split along various characters at once? For example, to split on the condition of there being $$$ or @@@?
+What exactly is the function of "open". 
 '''
 krewes = {}
 file = open("krewes.txt", "r")
@@ -18,5 +24,4 @@ for person in alls:
         krewes[int(tempList[0])] = [tempList[1:]]
     else:
         krewes[int(tempList[0])].append(tempList[1:])
-    print(tempList)
 print(krewes)
