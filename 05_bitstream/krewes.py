2095b72434f8aa36722e14e36779524a46f4f502
Julia Lee <jlee30@stuy.edu>
2022-09-29 15:50:59 -0400

    fixed bug

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -14,7 +14,7 @@ string = file.read()
 alls = string.split("@@@")
 for person in alls:
     tempList = person.split("$$$")
-    if tempList[0] not in list(krewes.keys()):
+    if int(tempList[0]) not in list(krewes.keys()):
         krewes[int(tempList[0])] = [tempList[1:]]
     else:
         krewes[int(tempList[0])].append(tempList[1:])
