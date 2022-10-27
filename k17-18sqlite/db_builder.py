521767f2818408969127dca30cd85dccd2a2db2f
julia <jlee30@stuy.edu>
2022-10-26 22:48:23 -0400

    Added comments

--- k17-18sqlite/db_builder.py
+++ k17-18sqlite/db_builder.py
@@ -17,20 +17,24 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
-c.execute(".mode csv")
+# c.execute(".mode csv")
+''' You aren't able to change the mode with c.execute because c.execute is running commands with the database open in relation to a sepcific table. If you think about sqlite in the
+terminal, the mode is always changed outside of having the table open and it's meant to change how the table is stored. Thus, it's not actually runnable code in the same area that
+the create table or insert into commands are.
+'''
 
-# def createTable(csvFile):
-#     with open(csvFile, newline='\n') as csvfile:
-#         reader = csv.DictReader(csvfile)
-#         name = csvFile[:-4]
-#         command = "create table " + name + "(" + reader.fieldnames[0] + " text, " + reader.fieldnames[1] + " int, " + reader.fieldnames[2] + " int);"
-#         c.execute(command)    # run SQL statement
-#         for row in reader:
-#             command = "insert into " + name + " values('" + row[reader.fieldnames[0]] + "', " + row[reader.fieldnames[1]] + ", " + row[reader.fieldnames[2]] + ");"
-#             c.execute(command)
-#
-# createTable('students.csv')
-# createTable('courses.csv')
+def createTable(csvFile): #Takes in a csv file and creates a table based on the csv file you're reading data from.
+    with open(csvFile, newline='\n') as csvfile:
+        reader = csv.DictReader(csvfile)
+        name = csvFile[:-4] #creates table name based on csv file name.
+        command = "create table " + name + "(" + reader.fieldnames[0] + " text, " + reader.fieldnames[1] + " int, " + reader.fieldnames[2] + " int);" #plugs in fieldnames from reader. The first line of the csv file wil always be the list of fieldnames unless otherwise specified.
+        c.execute(command)    # run SQL statement
+        for row in reader: # you could also run the command only once by utilizing executescript and simply creating a longer string with all the relevant commands you want.
+            command = "insert into " + name + " values('" + row[reader.fieldnames[0]] + "', " + row[reader.fieldnames[1]] + ", " + row[reader.fieldnames[2]] + ");"
+            c.execute(command)
+
+createTable('students.csv') #creates student table in db
+createTable('courses.csv') #creates courses table in db
 #==========================================================
 
 db.commit() #save changes
