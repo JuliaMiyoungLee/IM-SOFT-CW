5187ea7afc2823d0388539e9ff0293fdd9e0a01b
Julia Lee <jlee30@stuy.edu>
2023-04-24 13:16:05 -0400

    initial

---
+++ 30_jsdraw/clickdraw.js
@@ -0,0 +1,26 @@
+var c = document.getElementById("slate");
+
+var ctx = c.getContext("2d");
+
+var mode = "rect";
+
+var toggleMode = () => {
+    console.log("toggling...");
+    if (mode == "rect") {
+        mode = "circle";
+    }
+    else {
+        mode = "rect";
+    }
+}
+
+// var drawRect = function(e) {
+//     var mouseX = 
+// }
+var draw = () => {
+    console.log("draw");
+}
+
+c.addEventListener("click",draw);
+var bToggler = document.getElementById("buttonToggle");
+bToggler.toggleMode;
